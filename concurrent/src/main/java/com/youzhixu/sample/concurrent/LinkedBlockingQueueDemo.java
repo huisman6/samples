5046ad611417439be9b2281eeac515d56b86ccbe
potent6 <potent6@aliyun.com>
2015-05-14 18:25:48 +0800

    commit

---
+++ concurrent/src/main/java/com/youzhixu/sample/concurrent/LinkedBlockingQueueDemo.java
@@ -0,0 +1,16 @@
+package com.youzhixu.sample.concurrent;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * @author huiman
+ * @createAt 2015年5月14日 下午4:20:42
+ * @since 1.0.0
+ * @Copyright (c) 2015, youzhixu.com All Rights Reserved.
+ */
+public class LinkedBlockingQueueDemo {
+	public static void main(String[] args) {
+		LinkedBlockingQueue<Integer> linkedQueue = new LinkedBlockingQueue<>();
+
+	}
+}
