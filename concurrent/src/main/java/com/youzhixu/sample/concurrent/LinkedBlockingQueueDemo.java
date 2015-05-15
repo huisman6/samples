385251240dad87214ea8d2008e3a745f4b34ab63
potent6 <potent6@aliyun.com>
2015-05-15 10:34:00 +0800

    linked

--- concurrent/src/main/java/com/youzhixu/sample/concurrent/LinkedBlockingQueueDemo.java
+++ concurrent/src/main/java/com/youzhixu/sample/concurrent/LinkedBlockingQueueDemo.java
@@ -1,6 +1,7 @@
 package com.youzhixu.sample.concurrent;
 
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author huiman
@@ -10,7 +11,56 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class LinkedBlockingQueueDemo {
 	public static void main(String[] args) {
-		LinkedBlockingQueue<Integer> linkedQueue = new LinkedBlockingQueue<>();
+		int queueSize = 10;
+		LinkedBlockingQueue<Integer> linkedQueue = new LinkedBlockingQueue<>(queueSize);
+		new Thread(new Consumer(linkedQueue)).start();
+		new Thread(new Producer(linkedQueue)).start();
+	}
 
+	private static class Producer implements Runnable {
+		private LinkedBlockingQueue<Integer> linkedBlockingQueue;
+
+		public Producer(LinkedBlockingQueue<Integer> linkedBlockingQueue) {
+			super();
+			this.linkedBlockingQueue = linkedBlockingQueue;
+		}
+
+		@Override
+		public void run() {
+			try {
+				int i = 1;
+				while (i++ <= 5) {
+					int item = ThreadLocalRandom.current().nextInt(5000);
+					linkedBlockingQueue.put(item);
+					System.out.println("item added:" + item);
+					Thread.sleep(5);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static class Consumer implements Runnable {
+		private LinkedBlockingQueue<Integer> linkedBlockingQueue;
+
+		public Consumer(LinkedBlockingQueue<Integer> linkedBlockingQueue) {
+			super();
+			this.linkedBlockingQueue = linkedBlockingQueue;
+		}
+
+		@Override
+		public void run() {
+			try {
+				int i = 1;
+				while (i++ <= 5) {
+					int item = linkedBlockingQueue.take();
+					System.out.println("item got:" + item);
+					Thread.sleep(1000);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
