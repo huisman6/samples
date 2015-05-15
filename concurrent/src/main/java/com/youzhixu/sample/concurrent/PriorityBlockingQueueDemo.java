1b4741774ce3f6cdcc61da60b9f12295a1dd9aa0
potent6 <potent6@aliyun.com>
2015-05-15 13:12:11 +0800

    heapify

---
+++ concurrent/src/main/java/com/youzhixu/sample/concurrent/PriorityBlockingQueueDemo.java
@@ -0,0 +1,20 @@
+package com.youzhixu.sample.concurrent;
+
+/**
+ * <p>
+ *
+ * </p>
+ * 
+ * @author huisman
+ * @createAt 2015年5月15日 下午1:03:23
+ * @since 1.0.0
+ * @Copyright (c) 2015, youzhixu.com All Rights Reserved.
+ */
+
+public class PriorityBlockingQueueDemo {
+
+	public static void main(String[] args) {
+
+	}
+
+}
