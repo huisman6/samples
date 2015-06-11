90adc7b4508d91a2ad13c9088ed96f80cf611df9
potent6 <potent6@aliyun.com>
2015-06-11 10:26:05 +0800

    <dependency> 			<groupId>org.apache.activemq</groupId> 			<artifactId>activemq-core</artifactId> 			<version>5.7.0</version> 		</dependency>
    		<dependency>
    			<groupId>org.apache.activemq</groupId>
    			<artifactId>activemq-pool</artifactId>
    			<version>5.11.1</version>
    		</dependency>

---
+++ component/src/main/java/com/youzhixu/component/activemq/ClusterTest.java
@@ -0,0 +1,14 @@
+package com.youzhixu.component.activemq;
+
+/**
+ * @author huisman
+ * @createAt 2015年6月11日 上午10:24:25
+ * @since 1.0.0
+ * @Copyright (c) 2015, youzhixu All Rights Reserved.
+ */
+public class ClusterTest {
+
+	public static void main(String[] args) {
+
+	}
+}
