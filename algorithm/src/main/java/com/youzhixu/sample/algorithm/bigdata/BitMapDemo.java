60b4f4093055ab0da4494b75a53a48aa12b99d32
potent6 <potent6@aliyun.com>
2015-06-25 09:46:55 +0800

    bit map

---
+++ algorithm/src/main/java/com/youzhixu/sample/algorithm/bigdata/BitMapDemo.java
@@ -0,0 +1,21 @@
+package com.youzhixu.sample.algorithm.bigdata;
+
+import java.io.Serializable;
+
+/**
+ * @author huisman
+ * @createAt 2015年6月25日 上午9:46:03
+ * @since 1.0.0
+ * @Copyright (c) 2015, youzhixu.com All Rights Reserved.
+ */
+
+public class BitMapDemo {
+
+	public static void main(String[] args) {
+
+	}
+
+	class BitMap implements Serializable {
+
+	}
+}
