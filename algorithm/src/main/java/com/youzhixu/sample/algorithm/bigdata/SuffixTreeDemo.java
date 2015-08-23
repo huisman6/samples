6183945de9a2d5b4a80d6646f377d9722608ca74
potent6 <potent6@aliyun.com>
2015-08-23 08:30:38 +0800

    Ukkonen suffiX TREE

---
+++ algorithm/src/main/java/com/youzhixu/sample/algorithm/bigdata/SuffixTreeDemo.java
@@ -0,0 +1,17 @@
+package com.youzhixu.sample.algorithm.bigdata;
+
+/**
+ * @author huisman 
+ * @since 1.0.0
+ * @createAt 2015年7月4日 上午9:03:52
+ * @Copyright (c) 2015, youzhixu.com Rights Reserved. 
+ */
+public class SuffixTreeDemo {
+	public static void main(String[] args) {
+
+	}
+
+	
+}
+
+
